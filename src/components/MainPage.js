abfac3efc64bb25bdb3dc412a1761b2cbff1b5d5
david11yf29 <david11yf29@gmail.com>
2021-03-30 20:59:29 +0800

    updated installed typescript

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -22,7 +22,7 @@ class MainPage extends Component {
   };
 
   render() {
-    const { robots, onSearchChange, isPending } = this.props;
+    const { onSearchChange, isPending } = this.props;
 
     return (
       <div className="tc">
